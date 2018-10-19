9f077d2d5f6d7dd42b3df95320221b86ab085ae2
ophir <pere.jobs@gmail.com>
2018-10-19 22:24:26 +0200

    improve precision

--- webworker.js
+++ webworker.js
@@ -4,6 +4,6 @@ onmessage = function(e) {
   var points = e.data;
   var i = 0;
   postMessage(
-    solve(points, 1-1e-6, o => (i++)%500000 === 0 && postMessage(o))
+    solve(points, 1-5e-7, o => (i++)%1e6 === 0 && postMessage(o))
   );
 }
